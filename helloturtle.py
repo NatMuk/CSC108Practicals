cc2ac8c18e4a882de330a72a2f0e961b5368bde1
csc108 <nmukena@live.fr>
2015-09-07 16:58:34 -0700

    Practical1

---
+++ helloturtle.py
@@ -0,0 +1,8 @@
+__author__ = 'student'
+
+import turtle
+
+t = turtle.Turtle()
+t.forward(100)
+t.right(90)
+t.forward(100)
---
+++ helloturtlecolor.py
@@ -0,0 +1,15 @@
+__author__ = 'student'
+
+import turtle
+t = turtle.Turtle()
+t.color("red")
+t.forward(50)
+t.left(90)
+t.color("blue")
+t.forward(50)
+t.left(90)
+t.color("green")
+t.forward(50)
+t.left(90)
+t.color("yellow")
+t.forward(50)
